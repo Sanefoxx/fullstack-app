6d583e46dcce4ddbeb8c372a5a60ce7b7cd222b1
Sanefoxx <alexndr.lisitsin@gmail.com>
2022-04-27 14:07:18 +0300

    tried react features: useState and useEffect

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,12 +1,25 @@
+import { useState, useEffect} from "react";
 import './App.css';
 
 import {getAllStudents} from "./client";
 
 function App() {
-  getAllStudents()
-      .then(res => res.json())
-      .then(console.log)
-  return "hello react";
+  const  [students, setStudents] = useState([]);
+
+  const fetchStudents = () =>
+      getAllStudents()
+          .then(res => res.json())
+          .then(data => {
+            console.log(data);
+            setStudents(data)
+          })
+
+  useEffect(() => {
+      console.log("component is mounted");
+      fetchStudents();
+  }, []);
+
+  return <p>{students.length}</p>;
 }
 
 export default App;
